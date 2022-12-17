64e0c7c8e4cf28d4178ddf78798630f5c6d485a4
MajorFox <57305689+MajorFox-MJ@users.noreply.github.com>
2022-12-17 14:48:14 +0100

    Update GetGameDetails.cs
    Updating the number of details in the GameDetails command

--- CommandExpansion/Commands/GetGameDetails.cs
+++ CommandExpansion/Commands/GetGameDetails.cs
@@ -24,10 +24,14 @@
 					 + $"<color=green>Round started :</color> {Statistics.CurrentRound.StartTimestamp}\n"
 					 + $"<color=green>Map seed :</color> [<color=red>{Map.Seed}</color>]\n\n"
 					 + $"<color=green>TotalScpKills :</color> {Statistics.CurrentRound.TotalScpKills} Kill{(Statistics.CurrentRound.TotalScpKills < 2 ? "" : "s")}\n"
-					 + $"<color=green>TotalScp0492Made :</color> {Statistics.CurrentRound.TotalScp0492Made} SCP-049-2\n"
+					 + $"<color=green>ZombiesChanged :</color> {Statistics.CurrentRound.ZombiesChanged} SCP-049-2\n"
 					 + $"<color=green>ScpsAlive :</color> {Statistics.CurrentRound.ScpsAlive} SCP{(Statistics.CurrentRound.ScpsAlive < 2 ? "" : "s")}\n"
-					 + $"<color=green>TotalEscapedClassD :</color> {Statistics.CurrentRound.TotalEscapedClassD} Class-D{(Statistics.CurrentRound.TotalEscapedClassD < 2 ? "" : "s")}\n"
-					 + $"<color=green>TotalEscapedScientists :</color> {Statistics.CurrentRound.TotalEscapedScientists} Scientist{(Statistics.CurrentRound.TotalEscapedScientists < 2 ? "" : "s")}\n"
+					 + $"<color=green>ChaosInsurgencyAlive :</color> {Statistics.CurrentRound.ChaosInsurgencyAlive} SCP{(Statistics.CurrentRound.ChaosInsurgencyAlive < 2 ? "" : "s")}\n"
+					 + $"<color=green>ClassDAlive :</color> {Statistics.CurrentRound.ClassDAlive} SCP{(Statistics.CurrentRound.ClassDAlive < 2 ? "" : "s")}\n"
+					 + $"<color=green>MtfAndGuardsAlive :</color> {Statistics.CurrentRound.MtfAndGuardsAlive} SCP{(Statistics.CurrentRound.MtfAndGuardsAlive < 2 ? "" : "s")}\n"
+					 + $"<color=green>ZombiesAlive :</color> {Statistics.CurrentRound.ZombiesAlive} SCP{(Statistics.CurrentRound.ZombiesAlive < 2 ? "" : "s")}\n"
+					 + $"<color=green>ClassDEscaped :</color> {Statistics.CurrentRound.ClassDEscaped} Class-D{(Statistics.CurrentRound.ClassDEscaped < 2 ? "" : "s")}\n"
+					 + $"<color=green>ScientistsEscaped :</color> {Statistics.CurrentRound.ScientistsEscaped} Scientist{(Statistics.CurrentRound.ScientistsEscaped < 2 ? "" : "s")}\n"
 					 + $"<color=green>TotalKilledPlayers :</color> {Statistics.CurrentRound.TotalKilledPlayers} Player{(Statistics.CurrentRound.TotalKilledPlayers < 2 ? "" : "s")}";
 			return true;
 		}
